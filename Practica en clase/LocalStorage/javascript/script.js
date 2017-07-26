40682e466205a1d4008db5b3b1c1f4eaa7cdc8e9
Merci13 <mrjorxe@gmail.com>
2017-07-26 16:31:27 -0600

    tercero
    se agrego el local storage

---
+++ Practica en clase/LocalStorage/javascript/script.js
@@ -0,0 +1,14 @@
+
+function guardarForm() {
+
+localStorage.setItem("nombre", document.getElementById('nombre').value);
+localStorage.setItem("apellido", document.getElementById('apellido').value);
+localStorage.setItem("telefono", document.getElementById('telf').value);
+localStorage.setItem("contraseña", document.getElementById('pwd').value);
+  
+}
+
+
+
+
+
